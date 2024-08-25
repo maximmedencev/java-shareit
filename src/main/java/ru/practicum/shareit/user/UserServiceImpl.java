b701c4675929bfac906cdcab2f5d16ec192b7c92
Maxim Medencev <maxim.medencev@yandex.ru>
2024-08-25 18:49:20 +0700

    Убрать неиспользуемый импорт и исправить naming error

--- src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
@@ -19,9 +19,9 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     private boolean isEmailValid(String email) {
-        Pattern VALID_EMAIL_ADDRESS_REGEX =
+        Pattern validEmailAddressRegex =
                 Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
+        Matcher matcher = validEmailAddressRegex.matcher(email);
         return matcher.matches();
     }
 
